714a8462a91ffbe204a05c52db4b9401e859db02
Faradila <faradilaalwi@gmail.com>
2022-11-13 19:19:24 +0700

    nemabhakan kurang

--- tugas.js
+++ tugas.js
@@ -4,9 +4,13 @@ const sum = (a,b)=> {
 const div = (a,b)=> {
     return a/b
 }
+const min=(a,b)=> {
+    return a-b
+}
 const kalkulasi = (kata, cb)=> {
     const result = kata + cb(10,11)
     console.log(result)
 }
 kalkulasi('tambah: ',sum)
+kalkulasi('kurang', min)
 kalkulasi('bagi: ', div)
